66faa8cd8a909df26b3d6552b742c9b5449db00b
omkarhcode <omkarh.code@gmail.com>
2024-01-12 19:51:23 +0530

    Add function to check if username is taken

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -1,3 +1,16 @@
 <?php
 
 declare(strict_types=1);
+
+function get_username_taken(object $pdo, string $username)
+{
+
+    $query = "SELECT username FROM users WHERE username = :%username;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
