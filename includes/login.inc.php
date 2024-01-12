a6d18df96bc5811d55b3bdf013566164b7ac7639
omkarhcode <omkarh.code@gmail.com>
2024-01-13 00:18:10 +0530

    Add login functionality to handle user authentication

---
+++ includes/login.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    try {
+        // Connection to database
+        require_once "dbh.inc.php";
+        require_once "login_model.inc.php";
+        require_once "login_controller.inc.php";
+
+
+        // Error Validations
+        $errors = [];
+
+        if (is_input_empty($username, $pwd,)) {
+            $errors["empty_input"] = "Fill in all fields!";
+        };
+
+        $result = get_user($pdo, $username);
+
+        if (is_username_wrong($result)) {
+            $errors["login_incorrect"] = "Incorrect login info!";
+        }
+
+        if (!is_username_wrong($result) && !is_password_wrong($pwd, $result['pwd'])) {
+            $errors["password_incorrect"] = "Incorrect password!";
+        }
+
+        require_once "config_session.inc.php";
+
+        if ($errors) {
+            $_SESSION['errors_signup'] = $errors;
+
+            header("Location: ../index.php");
+            die();
+        }
+
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $result['id'];
+        session_id($sessionId);
+    } catch (PDOException $e) {
+        die("Query Failed: " . $e->getMessage());
+    }
+} else {
+    header("Location: ../index.php");
+    die();
+}
