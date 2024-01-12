49dbdc1b74896ce7be796f66d971c9a88c830866
omkarhcode <omkarh.code@gmail.com>
2024-01-12 20:45:48 +0530

    Add check_signup_errors function to display error messages on signup view

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -1,3 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
+
+function check_signup_errors()
+{
+    if (isset($_SESSION['errors_signup'])) {
+        $errors = $_SESSION['errors_signup'];
+
+        echo "<br>";
+
+        foreach ($errors as $error) {
+            echo '<p class="error-message">' . $error . '</p>';
+        }
+
+        unset($_SESSION['errors_signup']);
+    }
+}
