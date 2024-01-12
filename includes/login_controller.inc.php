f733f459fe7f6f877ebe4ee9e8d8d00cf774a439
omkarhcode <omkarh.code@gmail.com>
2024-01-13 01:31:42 +0530

    Fix return statements in login_controller.inc.php

--- includes/login_controller.inc.php
+++ includes/login_controller.inc.php
@@ -18,7 +18,7 @@ function is_username_wrong(bool|array $results)
     if (!$results) {
         return true;
     } else {
-        false;
+        return false;
     }
 }
 
@@ -27,6 +27,6 @@ function is_password_wrong(string $pwd, string $hashedPwd)
     if (!password_verify($pwd, $hashedPwd)) {
         return true;
     } else {
-        false;
+        return false;
     }
 }
