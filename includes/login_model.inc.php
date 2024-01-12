5d392a0c6c70c49c1c63188098537b9a18cb9893
omkarhcode <omkarh.code@gmail.com>
2024-01-13 01:32:15 +0530

    Update SQL query in get_user function

--- includes/login_model.inc.php
+++ includes/login_model.inc.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 
 function get_user(object $pdo, string $username)
 {
-    $query = "SELECT username FROM users WHERE username = :username;";
+    $query = "SELECT * FROM users WHERE username = :username;";
 
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':username', $username);
