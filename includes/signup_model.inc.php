511a72bae0035f74850bd7b5268959f30b4d2511
omkarhcode <omkarh.code@gmail.com>
2024-01-12 19:55:45 +0530

    Fix SQL query parameter in signup_model.inc.php

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -5,10 +5,24 @@ declare(strict_types=1);
 function get_username_taken(object $pdo, string $username)
 {
 
-    $query = "SELECT username FROM users WHERE username = :%username;";
+    $query = "SELECT username FROM users WHERE username = :username;";
 
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+
+
+function get_email_registered(object $pdo, string $email)
+{
+
+    $query = "SELECT email FROM users WHERE email = :email;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':email', $email);
     $stmt->execute();
 
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
