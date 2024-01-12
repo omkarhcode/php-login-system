a799c5498b1c99b037ef17fbddd3df4202e71583
omkarhcode <omkarh.code@gmail.com>
2024-01-12 21:42:21 +0530

    Refactor signup form in section-sign-up.php

--- pages/home/section/section-sign-up.php
+++ pages/home/section/section-sign-up.php
@@ -7,13 +7,7 @@ require_once 'includes/signup_view.inc.php';
 <h3>Signup</h3>
 
 <form action="includes/signup.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <div class="password-container">
-        <input type="password" name="pwd" placeholder="Password" id="password-field">
-        <span toggle="#password-field" class="toggle-password"><i class="fa fa-eye"></i></span>
-    </div>
-    <input type="text" name="email" placeholder="E-mail">
-    <input type="text" name="phone" placeholder="Phone Number">
+    <?php signup_input() ?>
     <button>Signup</button>
 </form>
 
