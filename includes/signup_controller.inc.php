bdd21281b4dc18829fcf442354448df92d9c452e
omkarhcode <omkarh.code@gmail.com>
2024-01-12 19:51:06 +0530

    Add input validation and error handling in signup controller

--- includes/signup_controller.inc.php
+++ includes/signup_controller.inc.php
@@ -1,3 +1,30 @@
 <?php
 
 declare(strict_types=1);
+
+
+function is_input_empty(string $username, string  $pwd, string  $email, string  $phone)
+{
+    if (empty($username) || empty($pwd) || empty($email) || empty($phone)) {
+        header("Location: ../index.php?error=emptyfields");
+        die();
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../index.php?error=invalidemail");
+        die();
+    } else if (strlen($pwd) < 6) {
+        header("Location: ../index.php?error=passwordlength");
+        die();
+    } else if (strlen($phone) < 15) {
+        header("Location: ../index.php?error=phonelength");
+        die();
+    }
+}
+
+
+function is_user_taken(object $pdo, string $username)
+{
+    if (get_username_taken($pdo, $username)) {
+        header("Location: ../index.php?error=usernametaken");
+        die();
+    }
+}
