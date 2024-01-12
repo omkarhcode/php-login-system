1a29fc5604f17d376212727acd663132484a3977
omkarhcode <omkarh.code@gmail.com>
2024-01-13 00:57:11 +0530

    Fix session variable name in login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         require_once "config_session.inc.php";
 
         if ($errors) {
-            $_SESSION['errors_signup'] = $errors;
+            $_SESSION['errors_login'] = $errors;
 
             header("Location: ../index.php");
             die();
