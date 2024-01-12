0d80cd387d87694a03923f82e2ebf392733aa3c4
omkarhcode <omkarh.code@gmail.com>
2024-01-12 22:57:08 +0530

    Refactor signup_input function to improve readability and handle error conditions

--- includes/signup_view.inc.php
+++ includes/signup_view.inc.php
@@ -4,21 +4,40 @@ declare(strict_types=1);
 
 function signup_input()
 {
-    if (isset($_SESSION['signup_data']["username"]) && !isset($_SESSION["errors_signup"]["username_taken"])) {
+
+    if (
+        isset($_SESSION['signup_data']["username"]) &&
+        !isset($_SESSION["errors_signup"]["username_taken"])
+    ) {
         echo '<input type="text" name="username" placeholder="Username" value="' . $_SESSION['signup_data']["username"] . '">';
     } else {
         echo '<input type="text" name="username" placeholder="Username">';
     }
+
+
     echo '<div class="password-container">
             <input type="password" name="pwd" placeholder="Password" id="password-field">
             <span toggle="#password-field" class="toggle-password"><i class="fa fa-eye"></i></span>
         </div>';
-    if (isset($_SESSION['signup_data']["email"]) && !isset($_SESSION["errors_signup"]["email_registered"])) {
+
+
+    if (
+        isset($_SESSION['signup_data']["email"]) &&
+        !isset($_SESSION["errors_signup"]["email_registered"]) &&
+        !isset($_SESSION["errors_signup"]["invalid_email"])
+    ) {
         echo '<input type="text" name="email" placeholder="E-mail" value="' . $_SESSION['signup_data']["email"] . '">';
     } else {
         echo '<input type="text" name="email" placeholder="E-mail">';
     }
-    if (isset($_SESSION['signup_data']["phone"]) && !isset($_SESSION["errors_signup"]["phone_registered"])) {
+
+
+    if (
+        isset($_SESSION['signup_data']["phone"]) &&
+        !isset($_SESSION["errors_signup"]["phone_registered"]) &&
+        !isset($_SESSION["errors_signup"]["phone_length"]) &&
+        !isset($_SESSION["errors_signup"]["phone_alphabetic"])
+    ) {
         echo '<input type="text" name="phone" placeholder="Phone Number" value="' . $_SESSION['signup_data']["phone"] . '">';
     } else {
         echo '<input type="text" name="phone" placeholder="Phone Number">';
