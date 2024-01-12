90726688450dd617deb79227536ba864dce7027a
omkarhcode <omkarh.code@gmail.com>
2024-01-12 20:45:37 +0530

    Add function to check if phone number is already registered

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -14,8 +14,6 @@ function get_username_taken(object $pdo, string $username)
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $results;
 }
-
-
 function get_email_registered(object $pdo, string $email)
 {
 
@@ -23,6 +21,19 @@ function get_email_registered(object $pdo, string $email)
 
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':email', $email);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+
+function get_phone_registered(object $pdo, string $phone)
+{
+
+    $query = "SELECT phone FROM users WHERE phone = :phone;";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(':phone', $phone);
     $stmt->execute();
 
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
